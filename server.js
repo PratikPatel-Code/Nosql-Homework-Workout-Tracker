2298f22e805c33acc0135d140e01e5d36baeeac7
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-17 22:45:59 -0400

    Changed some names to match

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
